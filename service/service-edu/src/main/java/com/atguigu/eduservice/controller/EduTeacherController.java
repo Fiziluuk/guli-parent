ae4be01630d9d3f2b44514757615769dd103078f
kingdee <qingzhu_gao@kingdee.com>
2022-01-29 10:43:13 +0800

    添加启动类、swagger、配置类

--- service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,13 +1,28 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/eduteacher")
 public class EduTeacherController {
+
+    @Autowired
+    private EduTeacherService teacherService;
+
+    @GetMapping("findAll")
+    public List<EduTeacher> list(){
+        List<EduTeacher> result = teacherService.list(null);
+        return result;
+    }
 
 }
 
--- service/service-edu/src/main/java/com/atguigu/eduservice/service/EduTeacherService.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/service/EduTeacherService.java
@@ -6,4 +6,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface EduTeacherService extends IService<EduTeacher> {
 
+
+
 }
